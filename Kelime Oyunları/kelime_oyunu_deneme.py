fe3049956f977f761914104c76c99593e84a8103
yAquila <52707121+yAquila@users.noreply.github.com>
2020-09-22 13:01:13 +0300

    Create kelime_oyunu_deneme.py

---
+++ Kelime Oyunları/kelime_oyunu_deneme.py
@@ -0,0 +1,24 @@
+"""
+Kelime düzenleme
+"""
+# f = open("C:\\Users\\ezrea\\Documents\\GitHub\\python\\TR_Word_Correcter_Project\\TR_Word_Correcter\\words.txt", encoding="utf-8")
+# words = f.read()
+# word_list = words.split("\n")
+# for word in word_list:
+#     word = word.strip()
+#     if any([x in word for x in " âîû"]):
+#         word_list.remove(word)
+    
+# words = "\n".join(word_list)
+# f.close()
+# f = open("C:\\Users\\ezrea\\Documents\\GitHub\\python\\TR_Word_Correcter_Project\\TR_Word_Correcter\\words.txt", encoding="utf-8", mode="w")
+# f.write(words)
+# f.close()
+
+# f = open("C:\\Users\\ezrea\\Documents\\GitHub\\python\\TR_Word_Correcter_Project\\TR_Word_Correcter\\words.txt", encoding="utf-8")
+# print(f.read())
+# f.close()
+"""
+================================================================================
+"""
+
