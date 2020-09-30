cb12f0873413d1a47220bb30769848c9e42a9a9c
yAquila <52707121+yAquila@users.noreply.github.com>
2020-09-30 19:44:39 +0300

    Piramit solver tamamen bitti

--- Piramit/piramit_deneme.py
+++ Piramit/piramit_deneme.py
@@ -28,18 +28,6 @@ class Piramit:
             ihtimaller.append((sayi-i,i))
         return ihtimaller
 
-    def possible(self, grid, row, column, n):
-        if (row==(self.boyut-1)):
-            if n == 1: return False
-        
-        if column != row and column != 0:#son karede ve ilk karede değilse
-            if n not in set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) & set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]]) and n not in set([j[0] for i in grid[row] for j in i if len(j) == 1]):
-                pass
-        elif column == 0:#ilk karede ise 
-            return n in set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]])
-        elif column == row:#son karede ise
-            return n in set([j[0] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column-1]])                
-        
 
     def en_dusuk_sayida_ihtimal(self,grid):
         en_dusuk_ihtimal = (9,0,0)
@@ -60,51 +48,114 @@ class Piramit:
         for column in range(self.boyut):
             c = []
             for row in range(column+1):
-                c.append(set([1,2,3,4,5,6,7,8,9]))
+                c.append(0)
             grid.append(c)
-        grid[0][0] = set([4])
-        grid[2][0] = set([1])
-        grid[2][2] = set([7])
+        grid[0][0] = 8
+        grid[2][0] = 6
+        grid[2][2] = 8
+        grid[4][0] = 7
+        grid[4][2] = 5
+        grid[4][4] = 3
         # canvas.create_text(self.orta_nokta(0,0,40), text=list(grid[0][0])[0], font=tkFont.Font(family="Poppins",size=25))
         # canvas.create_text(self.orta_nokta(0,2,40), text=list(grid[2][0])[0], font=tkFont.Font(family="Poppins",size=25))
         # canvas.create_text(self.orta_nokta(2,2,40), text=list(grid[2][2])[0], font=tkFont.Font(family="Poppins",size=25))
         
         return grid
 
-    def solver(self,grid):
+    def possible(self, grid, row, column, n):
+        if not (row==(self.boyut-1)):
+            if n == 1: return False
         
+        if column != row and column != 0:#son karede ve ilk karede değilse
+            if grid[row][column+1] != 0 and grid[row-1][column] != 0:
+                if not ((abs(n-grid[row][column+1]) == grid[row-1][column]) or (n+grid[row][column+1] == grid[row-1][column])):
+                    return False
+            if grid[row][column+1] != 0:
+                if n in grid[row] or n == grid[row][column+1]+1 or n == grid[row][column+1]-1:
+                    return False
+            if grid[row-1][column] != 0:
+                if n not in [i[0] for i in self.tum_ihtimaller(grid[row-1][column],(row==(self.boyut-1)))]:
+                    return False
+            if grid[row][column-1] != 0 and grid[row-1][column-1] != 0:
+                if not ((abs(n-grid[row][column-1]) == grid[row-1][column-1]) or (n+grid[row][column-1] == grid[row-1][column-1])):
+                    return False
+            if grid[row][column-1] != 0:
+                if n in grid[row] or n == grid[row][column-1]+1 or n == grid[row][column-1]-1:
+                    return False
+            if grid[row-1][column-1] != 0:
+                if n not in [i[0] for i in self.tum_ihtimaller(grid[row-1][column-1],(row==(self.boyut-1)))]:
+                    return False
+            return True
+            # if n not in set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) & set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]]) and n not in set([j[0] for i in grid[row] for j in i if len(j) == 1]):
+            #     pass
+            
+        elif column == 0:#ilk karede ise 
+            if grid[row][column+1] != 0 and grid[row-1][column] != 0:
+                if not ((abs(n-grid[row][column+1]) == grid[row-1][column]) or (n+grid[row][column+1] == grid[row-1][column])):
+                    return False
+            if grid[row][column+1] != 0:
+                if n in grid[row] or n == grid[row][column+1]+1 or n == grid[row][column+1]-1:
+                    return False
+            if grid[row-1][column] != 0:
+                if n not in [i[0] for i in self.tum_ihtimaller(grid[row-1][column],(row==(self.boyut-1)))]:
+                    return False
+            return True
+            # return n in set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]])
+        elif column == row:#son karede ise
+            if grid[row][column-1] != 0 and grid[row-1][column-1] != 0:
+                if not ((abs(n-grid[row][column-1]) == grid[row-1][column-1]) or (n+grid[row][column-1] == grid[row-1][column-1])):
+                    return False
+            if grid[row][column-1] != 0:
+                if n in grid[row] or n == grid[row][column-1]+1 or n == grid[row][column-1]-1:
+                    return False
+            if grid[row-1][column-1] != 0:
+                if n not in [i[0] for i in self.tum_ihtimaller(grid[row-1][column-1],(row==(self.boyut-1)))]:
+                    return False
+            return True
+            # return n in set([j[0] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column-1]])                
+
+    def solver(self,grid):
+        # print(self.tum_ihtimaller(4,False))
         for row in range(1,self.boyut):
             for column in range(row+1):
-                if len(grid[row][column]) > 1:
-                    if column != row and column != 0:#son karede ve ilk karede değilse
-                        # print(set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) | set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]]))
-                        for n in range(1,10):#set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) & set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]]):    
-                            if n not in set([list(j)[0] for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]+1 for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]-1 for j in [i for i in grid[row]] if len(j) == 1]):
-                                if len(grid[row][column+1]) == 1 and len(grid[row-1][column]) == 1 and not ((abs(n-list(grid[row][column+1])[0]) == list(grid[row-1][column])[0]) or (n+list(grid[row][column+1])[0] == list(grid[row-1][column])[0])): continue
-                                if len(grid[row][column+1]) == 1 and len(grid[row-1][column]) == 1 and not ((abs(n-list(grid[row][column+1])[0]) == list(grid[row-1][column])[0]) or (n+list(grid[row][column+1])[0] == list(grid[row-1][column])[0])): continue
-                                grid[row][column] = set([n])
-                                self.solver(grid)
-                                grid[row][column] = set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) & set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]])
-                        return
-                    elif column == 0:#ilk karede ise 
-                        for n in range(1,10):#set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]):
-                            if n not in set([list(j)[0] for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]+1 for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]-1 for j in [i for i in grid[row]] if len(j) == 1]):
-                                if len(grid[row][column+1]) == 1 and len(grid[row-1][column]) == 1 and not ((abs(n-list(grid[row][column+1])[0]) == list(grid[row-1][column])[0]) or (n+list(grid[row][column+1])[0] == list(grid[row-1][column])[0])): continue
-                                grid[row][column] = set([n])
-                                self.solver(grid)
-                                grid[row][column] = set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]])
-                        return
-                    elif column == row:#son karede ise
-                        for n in range(1,10):#set([j[0] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column-1]]):             
-                            if n not in set([list(j)[0] for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]+1 for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]-1 for j in [i for i in grid[row]] if len(j) == 1]):
-                                if len(grid[row][column-1]) == 1 and len(grid[row-1][column-1]) == 1 and not ((abs(n-list(grid[row][column-1])[0]) == list(grid[row-1][column-1])[0]) or (n+list(grid[row][column-1])[0] == list(grid[row-1][column-1])[0])): continue
-                                grid[row][column] = set([n])
-                                self.solver(grid)
-                                grid[row][column] = set([j[0] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column-1]])
-                        return
+                if grid[row][column] == 0:
+                    for n in range(1,10):
+                        if self.possible(grid, row, column, n):
+                            grid[row][column] = n
+                            self.solver(grid)
+                            grid[row][column] = 0
+                    return
+        print(grid)
+                # if len(grid[row][column]) > 1:
+                #     if column != row and column != 0:#son karede ve ilk karede değilse
+                #         # print(set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) | set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]]))
+                #         for n in set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) & set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]]):    
+                #             if n not in set([list(j)[0] for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]+1 for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]-1 for j in [i for i in grid[row]] if len(j) == 1]):
+                #                 if len(grid[row][column+1]) == 1 and len(grid[row-1][column]) == 1 and not ((abs(n-list(grid[row][column+1])[0]) == list(grid[row-1][column])[0]) or (n+list(grid[row][column+1])[0] == list(grid[row-1][column])[0])): continue
+                #                 if len(grid[row][column-1]) == 1 and len(grid[row-1][column-1]) == 1 and not ((abs(n-list(grid[row][column-1])[0]) == list(grid[row-1][column-1])[0]) or (n+list(grid[row][column-1])[0] == list(grid[row-1][column-1])[0])): continue
+                #                 grid[row][column] = set([n])
+                #                 self.solver(grid)
+                #                 grid[row][column] = set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]) & set([j[1] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[0] in grid[row][column-1]])
+                #         return
+                #     elif column == 0:#ilk karede ise 
+                #         for n in set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]]):
+                #             if n not in set([list(j)[0] for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]+1 for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]-1 for j in [i for i in grid[row]] if len(j) == 1]):
+                #                 if len(grid[row][column+1]) == 1 and len(grid[row-1][column]) == 1 and not ((abs(n-list(grid[row][column+1])[0]) == list(grid[row-1][column])[0]) or (n+list(grid[row][column+1])[0] == list(grid[row-1][column])[0])): continue
+                #                 grid[row][column] = set([n])
+                #                 self.solver(grid)
+                #                 grid[row][column] = set([j[0] for i in grid[row-1][column] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column+1]])
+                #         return
+                #     elif column == row:#son karede ise
+                #         for n in set([j[0] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column-1]]):             
+                #             if n not in set([list(j)[0] for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]+1 for j in [i for i in grid[row]] if len(j) == 1]) and n not in set([list(j)[0]-1 for j in [i for i in grid[row]] if len(j) == 1]):
+                #                 if len(grid[row][column-1]) == 1 and len(grid[row-1][column-1]) == 1 and not ((abs(n-list(grid[row][column-1])[0]) == list(grid[row-1][column-1])[0]) or (n+list(grid[row][column-1])[0] == list(grid[row-1][column-1])[0])): continue
+                #                 grid[row][column] = set([n])
+                #                 self.solver(grid)
+                #                 grid[row][column] = set([j[0] for i in grid[row-1][column-1] for j in self.tum_ihtimaller(i,(row==(self.boyut-1))) if j[1] in grid[row][column-1]])
+                #         return
         
 
-        print(grid)
+        
                     
                 # if len(grid[row][column]) == 0: return "Wrong Question"
                 # if len(grid[row][column]) == 1: continue
@@ -150,5 +201,5 @@ class Piramit:
         # root.geometry("400x250+300+300")
         # root.mainloop()
 soru = Piramit()
-soru.boyut = 3
+soru.boyut = 5
 soru.main()
