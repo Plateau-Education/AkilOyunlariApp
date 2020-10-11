bd8a77aa224087b185de59d5114e3dd159ad8875
yAquila <52707121+yAquila@users.noreply.github.com>
2020-10-11 22:31:04 +0300

    Hazine Avı devam

--- Hazine Avı/hazine_avi_deneme2.py
+++ Hazine Avı/hazine_avi_deneme2.py
@@ -2,6 +2,7 @@ import random, time, timeit, copy
 from tkinter import Tk, Canvas, BOTH
 import tkinter.font as tkFont
 import numpy as np
+from itertools import combinations
 
 class HazineAvi:
 
@@ -44,35 +45,132 @@ class HazineAvi:
                     canvas.create_line(row*genislik+10,column*genislik+10,(row+1)*genislik+10,(column+1)*genislik+10)
                     canvas.create_line(row*genislik+10,(column+1)*genislik+10,(row+1)*genislik+10,column*genislik+10)
      
-
-    def count01(self,y,x,grid):
+    def komsular(self,y,x):
         komsular = [(y+1,x), (y-1,x), (y,x+1), (y,x-1),
                     (y+1,x+1), (y+1,x-1), (y-1,x+1), (y-1,x-1)]
         for _ in range(5):
             for komsu in komsular:
                 if komsu[0] < 0 or komsu[0] > self.boyut-1 or komsu[1] < 0 or komsu[1] > self.boyut-1:
                     komsular.remove(komsu)
+        return komsular
+
+    def count01(self,y,x,grid):
+        komsular = self.komsular(y,x)
         list1 = []
         list0 = []
         for komsu in komsular:
             if grid[komsu[0]][komsu[1]] == -1: list1.append((komsu[0],komsu[1]))
             elif grid[komsu[0]][komsu[1]] == 0: list0.append((komsu[0],komsu[1]))
-        return len(list0), len(list1), list0, list1
+        return len(list0), len(list1), list0, list1, grid[y][x] == len(list1)
+
+    def minmax(self,c):
+        if c < 2: 
+            minc = 0
+            maxc = c+2
+        elif c > self.boyut -3:
+            minc = c-2
+            maxc = self.boyut-1
+        else:
+            minc = c-2
+            maxc = c+2
+        return minc,maxc
+
+    def intercept_setList(self,setList):
+        s = setList[0]
+        for i in setList[1:]:
+            s = s & i
+        return s
+
+    def possible_combinations(self,y,x,n,list0,list1,grid):
+        intercept_list = []
+        for c1 in combinations(list0,n-len(list1)):
+            comb_set = set()
+            copy_g = copy.deepcopy(grid)
+            impflag = False
+            minx,maxx,miny,maxy = self.boyut,0,self.boyut,0
+            for y1,x1 in c1:
+                copy_g[y1][x1] = -1
+                comb_set.add((y1,x1,-1))
+                if y1 < miny: miny = self.minmax(y1)[0]
+                if y1 > maxy: maxy = self.minmax(y1)[1]
+                if x1 < minx: minx = self.minmax(x1)[0]
+                if x1 > maxx: maxx = self.minmax(x1)[1]
+        
+            for c2 in combinations(list0,len(list0)-n+len(list1)):
+                for y2,x2 in c2:
+                    copy_g[y2][x2] = -2
+                    comb_set.add((y2,x2,-2))
+                    if y2 < miny: miny = self.minmax(y2)[0]
+                    if y2 > maxy: maxy = self.minmax(y2)[1]
+                    if x2 < minx: minx = self.minmax(x2)[0]
+                    if x2 > maxx: maxx = self.minmax(x2)[1]
+            
+            for y3 in range(miny,maxy+1):
+                if impflag: break
+                for x3 in range(minx,maxx+1):
+                    if impflag: break
+                    n1 = copy_g[y3][x3]
+                    if n1 > 0:
+                        count_0, count_1, list_0 = self.count01(y3,x3,copy_g)[:3]
+                        if (n-count_1) > count_0: impflag = True
+                        elif (n-count_1) == 0:
+                            for y4,x4 in list_0:
+                                copy_g[y4][x4] = -2
+                                comb_set.add((y4,x4,-2))
+                        elif (n-count_1) == count_0:
+                            for y5,x5 in list_0:
+                                copy_g[y5][x5] = -1
+                                comb_set.add((y5,x5,-1))
+            # print(comb_set)
+            if impflag: break
+            else:
+                intercept_list.append(comb_set)
+        print(intercept_list)
+        if intercept_list:
+            return self.intercept_setList(intercept_list)
+        else:
+            return set() 
+        
+            # komsular = self.komsular(y1,x1)
+            # for k in komsular:
+            #     if grid[k[0]][k[1]] > 0:
+            #         komsular2 = self.komsular(k[0],k[1])
+            #         if (all([self.count01(y2,x2,grid)[-1] == False for y2,x2 in komsular2 if grid[y2][x2] > 0]) and any([grid[y3][x3] > 0 for y3,x3 in komsular2])):
+            #             impflag = False
+            #             c0,c1,l0 = self.count01(k[0],k[1],grid)[:3]
+            #             # bi dict yapılabilir. içinde her kombinasyonun her komşu sayı için olabilecek kombinasyonları olur. en son bunlar set intercept yapılır.
+            #         else:
+            #             impflag = True
+            #             break
+
+                        
+        # comb2 = set()
+        # for combination2 in combinations(list0,len(list0)-n+len(list1)):
+        #     comb2.add(combination2)
+
+    def first_base(self,grid):
+        for y in range(self.boyut):
+            for x in range(self.boyut):
+                n = grid[y][x]
+                if n > 0:
+                    count0, count1, list0, list1 = self.count01(y,x,grid)[:4]
+                    if (n-count1) > count0: return "Wrong Question"
+                    elif (n-count1) == 0:
+                        for y2,x2 in list0:
+                            grid[y2][x2] = -2
+                    elif (n-count1) == count0:
+                        for y3,x3 in list0:
+                            grid[y3][x3] = -1
+                    elif (n-count1) < count0:
+                        for pc in self.possible_combinations(y,x,n,list0,list1,grid):
+                            if pc == set(): continue
+                            y4,x4,n4 = pc
+                            grid[y4][x4] = n4
 
     def solver2(self,grid):
         for _ in range(10):
-            for y in range(self.boyut):
-                for x in range(self.boyut):
-                    n = grid[y][x]
-                    if n > 0:
-                        count0, count1, list0, list1 = self.count01(y,x,grid)
-                        if (n-count1) > count0: return "Wrong Question"
-                        elif (n-count1) == 0:
-                            for y2,x2 in list0:
-                                grid[y2][x2] = -2
-                        elif (n-count1) == count0:
-                            for y3,x3 in list0:
-                                grid[y3][x3] = -1
+            self.first_base(grid)
+
         self.solutions.append(copy.deepcopy(grid))                    
 
     def main(self):
--- temp.py
+++ temp.py
@@ -1,8 +1,31 @@
-# import numpy as np
+# # import numpy as np
 
-# a = np.zeros((8,8),dtype=int)
+# # a = np.zeros((8,8),dtype=int)
 
 import itertools
 
-for i in itertools.permutations([1,2,3],2):
-    print(i)
+# # for i in itertools.combinations([(3,2),(4,3),(5,3),(5,2)],3):
+# #     print(i)
+
+
+# def intercept_setList(setList):
+#     s = setList[0]
+#     for i in setList[1:]:
+#         s = s & i
+#     return s
+
+# def common_coordinates(tuple12):
+#     one = intercept_setList(tuple12[0])
+#     two = intercept_setList(tuple12[1])
+#     return one,two
+
+# d = {(1,1) : ([{(1,2),(2,3)},{(1,2),(3,4),(5,3)},{(1,2)}], [{(3,2),(5,3)},{(3,2),(3,4),(5,3)},{(3,2),(5,3)}])}
+# # tuple12 = ([{(1,2),(2,3)},{(1,2),(3,4),(5,3)},{(1,2)}], [{(3,2),(5,3)},{(3,2),(3,4),(5,3)},{(3,2),(5,3)}])
+# d[(1,1)] = common_coordinates(d[(1,1)])
+# print(d)
+
+# a = [{(1,2),(2,3)},{(1,2),(3,4),(5,3)},{(1,2)}]
+# print(intercept_setList(a))
+# list0 = [(2, 0), (0, 0), (1, 1), (0, 1)]
+# for y1 in itertools.combinations(list0,1):
+#     print(y1)
