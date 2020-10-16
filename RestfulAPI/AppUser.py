0aa8f5bb3987b506c4274976cf36c84b77340cab
Yusuf Efdal Yılmaz <71921435+beystef@users.noreply.github.com>
2020-10-16 15:12:44 +0300

    API ve User eklendi.
    Pycharm projesi olarak açtım dosyayı o yüzden genel bi değişiklik olarak algıladı sanırsam. .idea klasörü pycharm için.

---
+++ RestfulAPI/AppUser.py
@@ -0,0 +1,84 @@
+import requests as r
+from json import dumps, loads
+import SayiBulmaca.SayiBulmaca_3 as SayiBulmaca_3
+import SayiBulmaca.SayiBulmaca_4 as SayiBulmaca_4
+import Sudoku.Sudoku_6x6 as Sudoku_6x6
+import Sudoku.Sudoku_9x9 as Sudoku_9x9
+urlSudoku6Kolay = "http://127.0.0.1:5000/"+"Sudoku_6_KOLAY"
+urlSayiBulmaca3 = "http://127.0.0.1:5000/"+"SayiBulmaca_3"
+urlSayiBulmaca4 = "http://127.0.0.1:5000/"+"SayiBulmaca_4"
+token = "fx!Ay:;<p6Q?C8N{"
+
+
+def PostGame(num, url1, req="SORU", token1=None, game=None, level=None):
+    data = []
+    if level:
+        for _ in range(num):
+            data.append(game.main(level))
+    else:
+        for _ in range(num):
+            data.append(game.main())
+    post = {"Info": dumps({"Data": data, "Req": req}), "Token": token1}
+    req = r.post(url1, data=post)
+    print(req.status_code, req.json())
+
+
+def GetGame(num=None, req="SORU", token1=None, url1=None, where=None):
+    get = {"Info": num, "Where": where, "Token": token1, "Req": req}
+    req = r.get(url1, data=get)
+    return req.status_code, req.json()
+
+
+def PrintSayiBulmaca(veri):
+    for a in veri:
+        for _ in a:
+            for j in _:
+                print(j)
+            print("------------------")
+
+
+def PrintSudoku(veri):
+    if len(veri[0][0]) == 6:
+        for a in veri:
+            for i in a:
+                row1, row2, row3, row4, row5, row6 = i
+                print(row1[0:3], row1[3:6])
+                print(row2[0:3], row2[3:6])
+                print('')
+                print(row3[0:3], row3[3:6])
+                print(row4[0:3], row4[3:6])
+                print('')
+                print(row5[0:3], row5[3:6])
+                print(row6[0:3], row6[3:6])
+                print("----------------------")
+            print("----------------------")
+    elif len(veri[0][0]) == 9:
+        for a in veri:
+            for i in a:
+                row1, row2, row3, row4, row5, row6, row7, row8, row9 = i
+                print(row1[0:3], row1[3:6], row1[6:9])
+                print(row2[0:3], row2[3:6], row2[6:9])
+                print(row3[0:3], row3[3:6], row3[6:9])
+                print('')
+                print(row4[0:3], row4[3:6], row4[6:9])
+                print(row5[0:3], row5[3:6], row5[6:9])
+                print(row6[0:3], row6[3:6], row6[6:9])
+                print('')
+                print(row7[0:3], row7[3:6], row7[6:9])
+                print(row8[0:3], row8[3:6], row8[6:9])
+                print(row9[0:3], row9[3:6], row9[6:9])
+                print("--------------------------------")
+            print("--------------------------------")
+
+
+PostGame(num=5, url1=urlSayiBulmaca4, token1=token, game=SayiBulmaca_4)
+info = GetGame(num="a", url1=urlSayiBulmaca4, token1=token)
+print(info[0], info[1])
+# res = loads(info[1])["Info"]
+# PrintSayiBulmaca(res)
+# for i in res:
+#     for j in i:
+#         print(j)
+#     print("------------------")
+# print(info[0])
+
---
+++ RestfulAPI/RestfulAPI.py
@@ -0,0 +1,125 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from json import loads, dumps
+import sqlite3
+
+app = Flask(__name__)
+api = Api(app)
+request = reqparse.RequestParser()
+request.add_argument("Info", help="Enter Info", required=True)
+request.add_argument("Token", required=False)
+request.add_argument("Where", required=False)
+request.add_argument("Req", required=False)
+db = sqlite3.connect("C:/Users/Proper/PycharmProjects/untitled/sorular.db", check_same_thread=False)
+cursor = db.cursor()
+
+
+def CloseDB():
+    db.close()
+
+
+def CreateTable(table_name):
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (ID INTEGER PRIMARY KEY AUTOINCREMENT,SORU VARCHAR(500))")
+    db.commit()
+    return
+
+
+def InsertInto(table_name, columns="SORU", values=None):
+    if columns == "SORU":
+        cursor.execute(f"INSERT INTO {table_name} ({columns}) VALUES (?)", (dumps(values),))
+        db.commit()
+        return
+    cursor.execute(f"INSERT INTO {table_name} ({columns}) VALUES (?,?)", (dumps(values[0]), dumps(values[1])))
+    db.commit()
+    return
+
+
+def SelectFrom(table_name, columns, amount=None, where="ID NOT NULL"):
+    if amount:
+        cursor.execute(f"""SELECT {columns} FROM {table_name} WHERE ID IN (SELECT ID FROM {table_name} 
+                        WHERE {where} ORDER BY RANDOM() LIMIT {amount}) ORDER BY RANDOM()""")
+        db.commit()
+    else:
+        cursor.execute(f"SELECT {columns} FROM {table_name} WHERE {where} ORDER BY RANDOM()")
+        db.commit()
+    sorular = []
+    for i in cursor.fetchall().copy():
+        row = []
+        for j in i:
+            row.append(loads(j))
+        sorular.append(row)
+    return sorular
+
+
+# def DeleteFrom(table_name, where=None):
+#     if where:
+#         cursor.execute(f"DELETE FROM ({table_name}) WHERE ({where})")
+#         db.commit()
+#     else:
+#         cursor.execute(f"DELETE FROM ({table_name})")
+#         db.commit()
+#     db.close()
+#     return
+
+
+class Sayi(Resource):
+    def __init__(self):
+        self.token = "fx!Ay:;<p6Q?C8N{"
+
+    def post(self, game):
+        args = request.parse_args()
+        if args["Token"] == self.token:
+            if args["Info"]:
+                data_set = SelectFrom(table_name=game, columns="SORU")
+                for s in data_set:
+                    data_set[data_set.index(s)] = str(s[0])
+                data = loads(args["Info"])["Data"]
+                for i in data:
+                    if type(i) == list:
+                        if str(i) not in data_set:
+                            InsertInto(table_name=game, columns=loads(args["Info"])["Req"], values=i)
+                return {"Data": "Created"}, 201
+            else:
+                return {"Message": "Unprocessable Entity"}, 422
+        else:
+            return {"Message": "Unauthorized"}, 401
+
+    def get(self, game):
+        args = request.parse_args()
+        if args["Token"] == self.token:
+            if args["Info"]:
+                try:
+                    num = int(args["Info"])
+                except ValueError:
+                    return {"Message": "Unprocessable Entity"}, 422
+                if args["Where"]:
+                    return dumps({"Info": SelectFrom(table_name=game, columns=args["Req"], amount=num, where=args["Where"])})
+                return dumps({"Info": SelectFrom(table_name=game, columns=args["Req"], amount=num)})
+            else:
+                if args["Where"]:
+                    return dumps({"Info": SelectFrom(table_name=game, columns=args["Req"], where=args["Where"])})
+                return dumps({"Info": SelectFrom(table_name=game, columns=args["Req"])})
+        else:
+            return {"Message": "Unauthorized"}, 401
+
+    # def delete(self, game, level):
+    #     num = request.parse_args()["info"]
+    #     if storage:
+    #         if num:
+    #             num = int(num)
+    #             for i in range(num):
+    #                 storage[game][level].remove(storage[game][level][0])
+    #             return {"Data": "Deleted"}, 201
+    #         else:
+    #             DeleteFrom(game, level)
+    #             return {"Data": "Deleted"}, 201
+    #     else:
+    #         return {"Data": "Ulaşılamadı"}
+
+    # def patch(self, game, level):
+    #     pass
+
+
+api.add_resource(Sayi, "/<string:game>")
+if __name__ == '__main__':
+    app.run(debug=True)
