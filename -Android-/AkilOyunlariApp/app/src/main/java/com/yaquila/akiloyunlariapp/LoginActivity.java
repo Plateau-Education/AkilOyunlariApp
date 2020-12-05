acec4ef5fcda6600a38976d266550d46eb656f59
yAquila <52707121+yAquila@users.noreply.github.com>
2020-12-05 12:12:57 +0300

    Nickname added.

--- -Android-/AkilOyunlariApp/app/src/main/java/com/yaquila/akiloyunlariapp/LoginActivity.java
+++ -Android-/AkilOyunlariApp/app/src/main/java/com/yaquila/akiloyunlariapp/LoginActivity.java
@@ -3,15 +3,18 @@ package com.yaquila.akiloyunlariapp;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -30,7 +33,12 @@ import com.google.android.gms.tasks.Task;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
@@ -40,6 +48,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     GoogleSignInClient mGoogleSignInClient;
     String signInUpStatus = "SignUp";
     boolean alreadyHaveID = false;
+    boolean hasNicknameB = false;
+    EditText usernameET;
+    EditText passwordET;
+    LoadingDialog loadingDialog;
 
     public class PostRequest extends AsyncTask<String, Void, String> {
 
@@ -50,8 +62,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         @Override
         protected String doInBackground(String... strings) {
             try {
-                result = "{\""+strings[3]+"\":\""+ strings[1] + "\"}";
-                String URL = strings[0]+"?Token="+strings[2];
+                result = "{\""+strings[3]+"\":\""+ strings[1] + "\", \"Token\":"+ "\""+strings[2]+ "\"}";
+                String URL = strings[0];
                 Log.i("URL",URL);
                 requestQueue = Volley.newRequestQueue(getApplicationContext());
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
@@ -63,17 +75,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             resId = objres.getString("Message");
                             SharedPreferences sharedPreferences = getSharedPreferences("com.yaquila.akiloyunlariapp",MODE_PRIVATE);
                             sharedPreferences.edit().putString("id",resId).apply();
+
+                            GetRequest getRequest = new GetRequest();
+                            //noinspection deprecation
+                            getRequest.execute("https://akiloyunlariapp.herokuapp.com","fx!Ay:;<p6Q?C8N{");
+
                             Log.i("ResId",resId);
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            Toast.makeText(LoginActivity.this, "Wrong Username or Password", Toast.LENGTH_SHORT).show();
+                            usernameET.setText("");
+                            usernameET.setText("");
                         }
                         Log.i("Volley", response);
+
                     }
                 }, new Response.ErrorListener() {
                     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("Volley", error.getMessage()+"");
+                        try{
+                            loadingDialog.dismissDialog();
+                            if(error.getMessage()==null){
+                                Toast.makeText(LoginActivity.this, "This username is already taken. Please choose a different username.", Toast.LENGTH_LONG).show();
+                                usernameET.setText("");
+                                passwordET.setText("");
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.e("VolleyError", error.getMessage()+"");
                     }
                 }) {
                     @Override
@@ -96,15 +127,140 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             SharedPreferences sharedPreferences = getSharedPreferences("com.yaquila.akiloyunlariapp",MODE_PRIVATE);
             return sharedPreferences.getString("id","none");
         }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class GetRequest extends AsyncTask<String, Void, String> {
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
+        protected String doInBackground(String... strings) {
+            try {
+                StringBuilder result = new StringBuilder();
+                SharedPreferences sharedPreferences = getSharedPreferences("com.yaquila.akiloyunlariapp",MODE_PRIVATE);
+                String id = sharedPreferences.getString("id", "non");
+                URL reqURL = new URL(strings[0] + "/" + id + "?Token=" +strings[1]);
+                HttpURLConnection connection = (HttpURLConnection) reqURL.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.setDoOutput(false);
+                connection.connect();
+                InputStream in;
+                int status = connection.getResponseCode();
+                if (status != HttpURLConnection.HTTP_OK)  {
+                    in = connection.getErrorStream();
+                }
+                else  {
+                    in = connection.getInputStream();
+                }
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data != -1) {
+
+                    char current = (char) data;
+                    result.append(current);
+                    data = reader.read();
+                }
+                Log.i("result", result.toString());
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected void onPostExecute(String result) {
+            //noinspection deprecation
+            super.onPostExecute(result);
+
+            try {
+                org.json.JSONObject jb = new org.json.JSONObject(result.substring(result.indexOf("{"), result.lastIndexOf("}") + 1).replace("\\",""));
+                String id = jb.getString("_id");
+                Log.i("idFromJB",id);
+                try{
+                    String nick = jb.getString("\"Nick\"");
+                    Log.i("nickFromJB",nick+"");
+                    hasNicknameB = true;
+                } catch (Exception e){
+                    e.printStackTrace();
+                    hasNicknameB=false;
+                }
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                if(hasNicknameB)
+                    intent.putExtra("hasNickname","Has");
+                else
+                    intent.putExtra("hasNickname","Doesnt");
+                startActivity(intent);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            loadingDialog.dismissDialog();
         }
     }
 
+    public boolean isPasswordAndUsernameSuitable(String username, String password) {
+        if(username.length()==0 || password.length()==0){
+            return false;
+        } else if(username.length() < 4 || username.length() > 12 || password.length() < 4 || password.length() > 12){
+            Toast.makeText(this, "Username and password should be 4-12 words.", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (username.equals("favicon.ico")){
+            Toast.makeText(this, "Try another username.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void signClicked(View view){
+        String username = usernameET.getText().toString();
+        String password = passwordET.getText().toString();
+        if(!isPasswordAndUsernameSuitable(username,password)){
+            return;
+        }
+
+        if(signInUpStatus.equals("SignIn")){
+            PostRequest postRequest = new PostRequest();
+            String postMessage = null;
+            try {
+                loadingDialogFunc();
+                //noinspection deprecation
+                postMessage = postRequest.execute("https://akiloyunlariapp.herokuapp.com/" + username, password, "fx!Ay:;<p6Q?C8N{", "SignIn").get();
+                Log.i("postId", postMessage);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            PostRequest postRequest = new PostRequest();
+            String postMessage = null;
+            try {
+                loadingDialogFunc();
+                //noinspection deprecation
+                postMessage = postRequest.execute("https://akiloyunlariapp.herokuapp.com/" + username, password, "fx!Ay:;<p6Q?C8N{", "SignUp").get();
+                Log.i("postId", postMessage);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    public void loadingDialogFunc(){
+        loadingDialog = new LoadingDialog(LoginActivity.this, getLayoutInflater().inflate(R.layout.loading_dialog,null));
+        ((TextView)loadingDialog.dialogView.findViewById(R.id.loadingTextView2)).setText(R.string.PleaseWait);
+        loadingDialog.startLoadingAnimation();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -115,13 +271,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         SharedPreferences sharedPreferences = getSharedPreferences("com.yaquila.akiloyunlariapp",MODE_PRIVATE);
         if(!"nottaken".equals(sharedPreferences.getString("id","nottaken"))){
+            Log.i("user_ID", Objects.requireNonNull(sharedPreferences.getString("id", "nottaken")));
             alreadyHaveID = true;
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("hasNickname","Has");
             startActivity(intent);
             overridePendingTransition(R.anim.do_nothing,R.anim.do_nothing);
         }
 
+
         if(!alreadyHaveID) {
+            findViewById(R.id.signInChangeButton).setOnClickListener(this);
+            usernameET = findViewById(R.id.usernameEditText);
+            passwordET = findViewById(R.id.passwordEditText);
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestEmail()
                     .build();
@@ -166,6 +328,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 PostRequest postRequest = new PostRequest();
                 String postMessage = null;
                 try {
+                    loadingDialogFunc();
                     //noinspection deprecation
                     postMessage = postRequest.execute("https://akiloyunlariapp.herokuapp.com/" + account.getEmail(), account.getDisplayName(), "fx!Ay:;<p6Q?C8N{", "Google").get();
                     Log.i("postId", postMessage);
@@ -187,10 +350,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             if(signInUpStatus.equals("SignUp")){
                 signInUpStatus = "SignIn";
                 ((Button)findViewById(R.id.usernameJoinButton)).setText(R.string.SignIn);
+                ((TextView)findViewById(R.id.signInChangeButton)).setText(R.string.DontHaveAccount);
                 //TODO Change some UI maybe
             } else {
                 signInUpStatus = "SignUp";
                 ((Button)findViewById(R.id.usernameJoinButton)).setText(R.string.SignUp);
+                ((TextView)findViewById(R.id.signInChangeButton)).setText(R.string.HaveAccount);
                 //TODO Change some UI maybe
             }
         }
--- -Android-/AkilOyunlariApp/app/src/main/java/com/yaquila/akiloyunlariapp/MainActivity.java
+++ -Android-/AkilOyunlariApp/app/src/main/java/com/yaquila/akiloyunlariapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.yaquila.akiloyunlariapp;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -21,13 +25,41 @@ public class MainActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.enter, R.anim.exit);
     }
 
+    public void nicknameDialog(){
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View nicknameDialogView = factory.inflate(R.layout.nickname_dialog, null);
+        final AlertDialog nicknameDialog = new AlertDialog.Builder(this).create();
+        nicknameDialog.setView(nicknameDialogView);
+
+        nicknameDialogView.findViewById(R.id.nicknameButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nicknameET = nicknameDialogView.findViewById(R.id.nicknameET);
+                if(nicknameET.getText().length() == 0){
+                    Log.i(".",".");
+                } else if (nicknameET.getText().length() < 4 || nicknameET.getText().length() < 12){
+                    Toast.makeText(getApplicationContext(), "Nickname should be 4-12 words.", Toast.LENGTH_SHORT).show();
+                } else {
+                    nicknameDialog.dismiss();
+                }
+            }
+        });
+        nicknameDialog.show();
+    }
+
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent intent = getIntent();
+        if(Objects.equals(intent.getStringExtra("hasNickname"), "Has")){
+            Log.i("hasNickname","Has");
+        } else {
+//            Toast.makeText(this, "You dont have a nickname", Toast.LENGTH_SHORT).show();
+            nicknameDialog();
+        }
 //        try{
 //            Intent intent = getIntent();
 //            if(Objects.equals(intent.getStringExtra("Logged in"), "Logged in")){
