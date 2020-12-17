1e1d1985ac0636f9a69d8e4413f030c77bc96aa0
Yusuf Efdal Yılmaz <71921435+beystef@users.noreply.github.com>
2020-12-17 17:45:09 +0300

    Api Stat, Info Update

--- Api/app.py
+++ Api/app.py
@@ -7,21 +7,17 @@ import os
 import uuid
 from b64uuid import B64UUID
 from base64 import b85encode, b85decode
-from flask_mail import Mail, Message
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 key = os.environ.get("MONGO_URI")
 key2 = os.environ.get("MONGO_URI2")
 token = os.environ.get("TOKEN")
+mail_key = os.environ.get('SENDGRID_API_KEY')
 app = Flask(__name__, template_folder="templates")
 app.env = "development"
-app.config["DEBUG"] = True
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = os.environ.get("email_user")
-app.config['MAIL_PASSWORD'] = os.environ.get("email_password")
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-mail = Mail(app)
+email_user = os.environ.get("email_user")
+email_password = os.environ.get("email_password")
 api = Api(app)
 request = reqparse.RequestParser()
 request.add_argument("Info", required=False)
@@ -32,24 +28,30 @@ request.add_argument("Ids", required=False)
 cluster = MongoClient(key)
 cluster2 = MongoClient(key2)
 db = cluster["AkilOyunlariDB"]
-userdb = cluster2["AkilOyunlariUsers"]
-
-def randomN(n):
-    range_start = 10**(n-1)
-    range_end = (10**n)-1
-    return randint(range_start, range_end)
+userdb = cluster2["AkilOyunlariUsers"]["Users"]
 
 
 def send_Mail(receiver, name):
-    kod = randomN(6)
+    kod = randint(100000, 1000000)
     members = [[name, kod]]
-    msg = Message('Mind Puzzles', sender=os.environ.get("email_user"), recipients=[receiver])
-    msg.html = str(render_template("email.htm", members=members))
-    mail.send(msg)
+    message = Mail(
+    from_email='akiloyunlariapp@gmail.com',
+    to_emails=f'{receiver}',
+    subject='Welcome To Mind Puzzles',
+    html_content=f'{render_template("email.htm", members=members)}')
+    try:
+        sg = SendGridAPIClient(mail_key)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(str(e))
     return kod
 
+
 def isTaken(info, query):
-    col = userdb["User_ids"]
+    col = userdb
     qs = [i[query] for i in col.find()]
     for q in qs:
         if info == q:
@@ -58,12 +60,13 @@ def isTaken(info, query):
 
 
 def signIn(email, password):
-    col = userdb["User_ids"]
-    ids = [(i["email"], i["password"], i["_id"]) for i in col.find()]
+    col = userdb
+    ids = [(i["email"], i["password"], i["_id"], i["username"], i["displayname"]) for i in col.find()]
     for idx in ids:
         if email == idx[0]:
             if password == idx[1]:
-                return {"Message": idx[2]}, 200
+                return {"Id": idx[2], "Username": b85decode(idx[3].encode()).decode(),
+                        "Displayname": b85decode(idx[4].encode()).decode()}, 200
     return False
 
 
@@ -72,47 +75,37 @@ def signUp(displayname, username, email, password):
     idx = None
     while flag:
         idx = B64UUID(uuid.uuid4()).string
-        flag = isTaken(idx, "_id")                   
-    userdb["Users"].insert_one({"_id": idx, "username": username, "solved": {
-        "Sudoku": {"6": {"Easy": [], "Medium": [], "Hard": []}, "9": {"Easy": [],
-                                                                        "Medium": [], "Hard": []}},
-        "SozcukTuru": {"Easy": [], "Medium": [], "Hard": [], "Hardest": []},
-        "SayiBulmaca": {"3": [], "4": [], "5": []}, "Patika": {"5": [], "7": [], "9": []},
-        "HazineAvi": {"5": [],
-                        "8": [], "10": []}, "Piramit": {"3": [], "4": [], "5": [], "6": []},
-        "Pentomino": {"Easy": [],
-                        "Medium": [], "Hard": []}, "Anagram": {"Easy": [], "Medium": [], "Hard": []}},
-                                "puan": {"Sudoku": 0, "SayiBulmaca": 0, "Piramit": 0, "Patika": 0, "HazineAvi": 0, "Pentomino": 0,
-                                         "SozcukTuru": 0, "Anagram":0,}})
-    userdb["User_ids"].insert_one({"_id": idx, "displayname": displayname, "username": username, "email": email, "password": password})
-    userdb["User_ids"].find_one_and_update({"_id": "userid"}, update={"$inc": {"seq": 1}}, new=True)
-    return idx
+        flag = isTaken(idx, "_id")
+    userdb.insert_one(
+        {"_id": idx, "displayname": displayname, "username": username, "email": email, "password": password,
+         "solved": {
+             "Sudoku": {"6": {"Easy": [[], [[0, 0]], 0, 0], "Medium": [[], [[0, 0]], 0, 0], "Hard": [[], [[0, 0]], 0, 0]},
+                        "9": {"Easy": [[], [[0, 0]], 0, 0], "Medium": [[], [[0, 0]], 0, 0], "Hard": [[], [[0, 0]], 0, 0]}},
+             "SozcukTuru": {"Easy": [[], [[0, 0]], 0, 0], "Medium": [[], [[0, 0]], 0, 0], "Hard": [[], [[0, 0]], 0, 0],
+                            "Hardest": [[], [[0, 0]], 0, 0]},
+             "SayiBulmaca": {"3": [[], [[0, 0]], 0, 0], "4": [[], [[0, 0]], 0, 0], "5": [[], [[0, 0]], 0, 0]},
+             "Patika": {"5": [[], [[0, 0]], 0, 0], "7": [[], [[0, 0]], 0, 0], "9": [[], [[0, 0]], 0, 0]},
+             "HazineAvi": {"5": [[], [[0, 0]], 0, 0], "8": [[], [[0, 0]], 0, 0], "10": [[], [[0, 0]], 0, 0]},
+             "Piramit": {"3": [[], [[0, 0]], 0, 0], "4": [[], [[0, 0]], 0, 0], "5": [[], [[0, 0]], 0, 0],
+                         "6": [[], [[0, 0]], 0, 0]},
+             "Pentomino": {"Easy": [[], [[0, 0]], 0, 0], "Medium": [[], [[0, 0]], 0, 0], "Hard": [[], [[0, 0]], 0, 0]},
+             "Anagram": {"Easy": [[], [[0, 0]], 0, 0], "Medium": [[], [[0, 0]], 0, 0], "Hard": [[], [[0, 0]], 0, 0]}},
+         "puan": {"Sudoku": 0, "SayiBulmaca": 0, "Piramit": 0, "Patika": 0,
+                  "HazineAvi": 0, "Pentomino": 0, "SozcukTuru": 0, "Anagram": 0}})
+    userdb.find_one_and_update({"_id": "userid"}, update={"$inc": {"seq": 1}}, new=True)
+    return idx 
 
 
 def getSolved(user_id, query):
     query = query.split(".")
     query.insert(0, "solved")
-    ids = userdb["Users"].find_one({"_id": user_id})
-    for key in query:
-        ids = ids[key]
+    query.append(0)
+    ids = userdb.find_one({"_id": user_id})
+    for k in query:
+        ids = ids[k]
     for n, i in enumerate(ids):
         ids[n] = int(i[0])
     return ids
-
-
-def getNextSeqValue(name, collection):
-    chose = collection.find_one_and_update(filter={"_id": name}, update={"$inc": {"seq": 1}}, new=True)
-    return chose["seq"]
-
-
-def Insert(col, value, cevap=None):
-    collection = db[col]
-    if cevap:
-        num = getNextSeqValue("userid", collection)
-        collection.insert_one({"_id": num, "soru": value[0], "cevap": value[1]})
-    else:
-        num = getNextSeqValue("userid", collection)
-        collection.insert_one({"_id": num, "soru": value})
 
 
 def Find(col, userid, amount=1, cevap=None, query=None):
@@ -140,15 +133,16 @@ def Find(col, userid, amount=1, cevap=None, query=None):
 def get1():
     collist = db.list_collection_names()
     collist.sort()
-    json = []
-    json.append(("Kullanıcı Sayısı", userdb["User_ids"].find_one(filter={"_id": "userid"})["seq"]))
+    json = [("Kullanıcı Sayısı", userdb.find_one(filter={"_id": "userid"})["seq"])]
     for i in collist:
         json.append((str(i), str(db[i].find_one(filter={"_id": "userid"})["seq"])))
     return render_template("index.html", members=json)
 
+
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
 
 
 class Games(Resource):
@@ -171,12 +165,29 @@ class Games(Resource):
 class User(Resource):
     def get(self, userinfo):
         args = request.parse_args()
-        if args["Token"] == token and args["Info"] and userinfo == "userGet":
-            json = args["Info"]
-            if json["Id"]:
-                user = userdb["Users"].find_one(filter={"_id": json["Id"]})
-                return dumps(user), 200
-            return {"Message": "Method not allowed"}, 405
+        if args["Token"] == token and args["Info"]:
+            cols = db.list_collection_names()
+            if userinfo == "userGet":
+                stat_dict = {}
+                usera = userdb.find_one({"_id": args["Info"]})["solved"]
+                for col in cols:
+                    userb = usera
+                    col2 = col.split(".")
+                    for i in col2:
+                        userb = userb[i]
+                    stat_dict[col] = userb[1]
+                stat_dict["puan"] = userdb.find_one({"_id": args["Info"]})["puan"]
+                return dumps(stat_dict), 200
+            elif userinfo == "userBest":
+                best_dict = {}
+                usera = userdb.find_one({"_id": args["Info"]})["solved"]
+                for col in cols:
+                    userb = usera
+                    col2 = col.split(".")
+                    for i in col2:
+                        userb = userb[i]
+                    best_dict[col] = [userb[2], userb[3]]
+                return dumps(best_dict), 200
         return {"Message": "Method not allowed"}, 405
 
     def post(self, userinfo):
@@ -185,54 +196,106 @@ class User(Resource):
             if userinfo == "userGoogle":
                 google = b85encode(args["Info"].encode()).decode()
                 if isTaken(google, "email"):
-                    return {"Message": userdb["User_ids"].find_one({"email": google})["_id"]}, 200
+                    idx = userdb.find_one({"email": google})
+                    return {"Id": idx["_id"], "Username": b85decode(idx["username"].encode()).decode(),
+                            "Displayname": b85decode(idx["displayname"].encode()).decode()}, 200
                 return {"Message": "Not Found"}, 200
             elif userinfo == "userSignIn":
                 json = loads(args["Info"].replace("'", '"'))
-                password = b85encode(json["password"].encode()).decode()  
-                email = b85encode(json["email"].encode()).decode()     
+                password = b85encode(json["password"].encode()).decode()
+                email = b85encode(json["email"].encode()).decode()
                 return signIn(email, password)
             elif userinfo == "userSignUp":
                 json = loads(args["Info"].replace("'", '"'))
-                username = b85encode(json["username"].encode()).decode() 
-                displayname = b85encode(json["displayname"].encode()).decode()  
-                password = b85encode(json["password"].encode()).decode()  
+                username = b85encode(json["username"].encode()).decode()
+                displayname = b85encode(json["displayname"].encode()).decode()
+                password = b85encode(json["password"].encode()).decode()
                 email = b85encode(json["email"].encode()).decode()
+                # kod = b85encode(json["code"].encode()).decode()
                 if not isTaken(email, "email"):
                     idx = signUp(displayname=displayname, username=username, email=email, password=password)
                     return {"Message": idx}, 200
                 return {"Message": "User Already Exists!"}, 200
             elif userinfo == "userSend":
-                json = loads(args["Info"].replace("'", '"'))                
+                json = loads(args["Info"].replace("'", '"'))
                 email = b85encode(json["email"].encode()).decode()
                 if not isTaken(email, "email"):
-                    kod = send_Mail(json["email"], json["displayname"])
-                    return {"Message": kod}, 200
+                    # kod = send_Mail(json["email"], json["displayname"])
+                    return {"Message": "Ok!"}, 200
                 return {"Message": "User Already Exists!"}, 200
             return {"Message": "Method not allowed"}, 405
         return {"Message": "Method not allowed"}, 405
 
     def put(self, userinfo):
-        puan_dict =  {"Sudoku.6.Easy": 0.1, "Sudoku.6.Medium": 0.4, "Sudoku.6.Hard": 0.6, "Sudoku.9.Easy": 0.3, "Sudoku.9.Medium": 0.8,
-                    "Sudoku.9.Hard": 1.3, "SayiBulmaca.3": 0.1, "SayiBulmaca.4": 0.3, "SayiBulmaca.5": 0.7, "Piramit.3": 0.1,
-                    "Piramit.4": 0.25, "Piramit.5": 0.6, "Piramit.6": 1.2, "Patika.5": 0.1, "Patika.7": 0.3, "Patika.9": 0.8,
-                    "HazineAvi.5": 0.1, "HazineAvi.8": 0.2, "HazineAvi.10": 0.7, "Pentomino.Easy": 0.1, "Pentomino.Medium": 0.3,
-                    "Pentomino.Hard": 1, "SozcukTuru.Easy": 0.1, "SozcukTuru.Medium": 0.25, "SozcukTuru.Hard": 0.5,
-                    "SozcukTuru.Hardest": 1, "Anagram.Easy": 0.1, "Anagram.Medium": 0.3, "Anagram.Hard": 0.8}
+        puan_dict = {"Sudoku.6.Easy": 0.1, "Sudoku.6.Medium": 0.4, "Sudoku.6.Hard": 0.6, "Sudoku.9.Easy": 0.3,
+                     "Sudoku.9.Medium": 0.8,
+                     "Sudoku.9.Hard": 1.3, "SayiBulmaca.3": 0.1, "SayiBulmaca.4": 0.3, "SayiBulmaca.5": 0.7,
+                     "Piramit.3": 0.1,
+                     "Piramit.4": 0.25, "Piramit.5": 0.6, "Piramit.6": 1.2, "Patika.5": 0.1, "Patika.7": 0.3,
+                     "Patika.9": 0.8,
+                     "HazineAvi.5": 0.1, "HazineAvi.8": 0.2, "HazineAvi.10": 0.7, "Pentomino.Easy": 0.1,
+                     "Pentomino.Medium": 0.3,
+                     "Pentomino.Hard": 1, "SozcukTuru.Easy": 0.1, "SozcukTuru.Medium": 0.25, "SozcukTuru.Hard": 0.5,
+                     "SozcukTuru.Hardest": 1, "Anagram.Easy": 0.1, "Anagram.Medium": 0.3, "Anagram.Hard": 0.8}
         args = request.parse_args()
         if args["Token"] == token and args["Info"] and userinfo == "userUpdate":
             info = loads(args["Info"].replace("'", '"'))
             idx = info["Id"]
-            game_ideas = info["Ids"]
+            game_ids = info["Ids"]
             total = 0
             if isTaken(idx, "_id"):
-                for n, i in enumerate(game_ideas):
-                    game_ideas[n] = i.split("-")
-                    if game_ideas[n][1]:
-                        total += puan_dict[info["Query"]] * 1000 / game_ideas[n][1]
-                game = info["Query"].split("-")[0]
-                userdb["Users"].find_one_and_update({"_id": idx},
-                                                    update={"$addToSet": {"solved." + info["Query"]: {"$each": info["Ids"]}}, "$inc": {f"puan.{game}": total}})
+                time = 0
+                new_time = 0
+                valid_ids = []
+                best = []
+                for n, i in enumerate(game_ids):
+                    game_ids[n] = i.split("-")
+                    game_ids[n][1] = int(game_ids[n][1])
+                    if game_ids[n][1]:
+                        time += 1
+                        new_time += game_ids[n][1]
+                        best.append(game_ids[n][1])
+                        total += puan_dict[info["Query"]] * 10
+                        valid_ids.append(game_ids[n][1])
+                best = min(best)
+                q = info['Query'].split(".")
+                q.append(3)
+                best_bef = userdb.find_one({"_id": idx})["solved"]
+                for i in q:
+                    best_bef = best_bef[i]
+                q.remove(3)
+                if best_bef != 0 and best_bef < best:
+                    best = best_bef
+                game = info["Query"].split(".")[0]
+                userdb.find_one_and_update({"_id": idx},
+                                           update={"$inc": {f"solved.{info['Query']}.2": total, f"puan.{game}": total}, 
+                                                   "$set": {f"solved.{info['Query']}.3": best}})
+                q.append(1)
+                ar = userdb.find_one({"_id": idx})["solved"]
+                for i in q:
+                    ar = ar[i]
+                count = ar[-1][1]
+                avg_t = ar[-1][0] * count
+                new_stats = [ar[-1]]
+                if count == 0:
+                    userdb.find_one_and_update({"_id": idx}, update={"$pull": {f"solved.{info['Query']}.1": [0, 0]}})
+                flag = True
+                if ar[-1][1] % 10 == 0:
+                    flag = False
+                for stat in valid_ids:
+                    count += 1
+                    avg_t += stat
+                    if count % 10 == 0:
+                        new_stats.append([avg_t / count, count])
+                if flag:
+                    userdb.find_one_and_update({"_id": idx}, update={"$pull": {f"solved.{info['Query']}.1": ar[-1]}})
+                if new_stats[-1][1] != count:
+                    print(avg_t, count)      
+                    new_stats.append([avg_t / count, count])
+                new_stats.remove(ar[-1])
+                userdb.find_one_and_update({"_id": idx},
+                                           update={"$push": {f"solved.{info['Query']}.1": {"$each": new_stats}},
+                                                   "$addToSet": {f"solved.{info['Query']}.0": {"$each": game_ids}}})
                 return {"Message": "Ok"}, 200
             return {"Message": "Not Found"}, 200
         return {"Message": "Method not allowed"}, 405
@@ -242,11 +305,9 @@ class User(Resource):
         if args["Token"] == token and args["Info"] and userinfo == "userDelete":
             idx = loads(args["Info"].replace("'", '"'))["Id"]
             if isTaken(idx, "_id"):
-                ids = userdb["User_ids"]
-                users = userdb["Users"]
-                ids.find_one_and_delete({"_id": idx})
+                users = userdb
                 users.find_one_and_delete({"_id": idx})
-                ids.find_one_and_update({"_id": "userid"}, update={"$inc": {"seq": -1}})
+                users.find_one_and_update({"_id": "userid"}, update={"$inc": {"seq": -1}})
                 return {"message": "User Deleted"}, 200
             return {"Message": "Not Found"}, 200
         return {"Message": "Method not allowed"}, 405
@@ -267,14 +328,19 @@ class LeaderBoard(Resource):
     def makeList(self):
         def sort(orn):
             return orn[1]
-        users = userdb["Users"]
+
+        users = userdb
         first = users.find()
         games = ["Sudoku", "SayiBulmaca", "Piramit", "Patika", "HazineAvi", "Pentomino", "SozcukTuru", "Anagram"]
         for game in games:
-            self.leader_list_dict[game] = [] 
+            self.leader_list_dict[game] = []
+        p = 0
         for i in first:
+            if p == 0:
+                p = 1
+                continue
             for game in games:
-                self.leader_list_dict[game].append([b85decode(i["username"].encode()).decode(), i[f"puan.{game}"]])
+                self.leader_list_dict[game].append([b85decode(i["username"].encode()).decode(), i["puan"][game]])
         for game in self.leader_list_dict.values():
             game.sort(key=sort, reverse=True)
 
