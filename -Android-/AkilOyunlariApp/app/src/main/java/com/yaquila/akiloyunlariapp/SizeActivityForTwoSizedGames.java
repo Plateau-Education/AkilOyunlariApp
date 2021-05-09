68b01fe18cd01b19ed7c6810b7cee85161e38af3
yAquila <52707121+yAquila@users.noreply.github.com>
2021-05-09 21:22:05 +0300

    Multiplayer mode first draft is finished. Debugging and adjustments are required.

--- -Android-/AkilOyunlariApp/app/src/main/java/com/yaquila/akiloyunlariapp/SizeActivityForTwoSizedGames.java
+++ -Android-/AkilOyunlariApp/app/src/main/java/com/yaquila/akiloyunlariapp/SizeActivityForTwoSizedGames.java
@@ -11,8 +11,11 @@ import android.widget.TextView;
 
 public class SizeActivityForTwoSizedGames extends AppCompatActivity {
 
+    String type;
+
     public void goToGameList(View view){
         Intent intent = new Intent(this, GameListActivity.class);
+        intent.putExtra("type",type);
         startActivity(intent);
         overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left);
     }
@@ -22,6 +25,10 @@ public class SizeActivityForTwoSizedGames extends AppCompatActivity {
         tvdiff.setBackground(getResources().getDrawable(R.drawable.clicked_diff_bg));
         tvdiff.setTextColor(getResources().getColor(R.color.f7f5fa));
         Intent intent = new Intent(this,DifficultyActivity.class);
+        if(type.contains("multi")){
+            intent = new Intent(getApplicationContext(), MultiplayerActivity.class);
+            intent.putExtra("pType",String.valueOf(type.charAt(type.length()-1)));
+        }
         intent.putExtra("gameName", view.getTag().toString());
         startActivity(intent);
         overridePendingTransition(R.anim.enter, R.anim.exit);
@@ -41,13 +48,15 @@ public class SizeActivityForTwoSizedGames extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_size_for_two_sized_games);
-
+        Intent intent = getIntent();
+        type = intent.getStringExtra("type");
         initDiffs();
     }
 
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(this, GameListActivity.class);
+        intent.putExtra("type",type);
         startActivity(intent);
         overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left);
     }
